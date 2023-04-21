87a08cf400cc2d14febe6d68a4c225a276d087fc
Ben <benhv@stud.ntnu.no>
2023-04-21 17:16:29 +0200

    now recommandations based on skin condition is shown

--- screens/User.tsx
+++ screens/User.tsx
@@ -12,14 +12,26 @@ import { db } from "../firebase";
 import { getAuth } from "firebase/auth";
 
 interface User {
-  skinType:string;
+  skinType: string;
   id: string;
+}
+
+interface ISkin {
+  type: string;
+  id: string;
+  description: string;
 }
 
 const User = () => {
   const [userSkins, setuserSkins] = React.useState<Array<User>>([]);
-  const skinDataRef  = collection(db, "skinData");
-  const userQuery = query(skinDataRef, where("userRef", "==", getAuth().currentUser?.uid),orderBy("timeStamp","desc"),limit(1));
+  const [skins, setSkins] = React.useState<Array<ISkin>>([]);
+  const skinDataRef = collection(db, "skinData");
+  const userQuery = query(
+    skinDataRef,
+    where("userRef", "==", getAuth().currentUser?.uid),
+    orderBy("timeStamp", "desc"),
+    limit(1)
+  );
 
   const fetchData = async () => {
     await getDocs(userQuery).then((querySnapshot) => {
@@ -31,22 +43,40 @@ const User = () => {
     });
   };
 
+  const fetchSkins = async () => {
+    await getDocs(collection(db, "skinCollection")).then((querySnapshot) => {
+      const data: any = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setSkins(data);
+    });
+  };
+
   React.useEffect(() => {
     fetchData();
-  }, [userQuery]);
-
+    fetchSkins();
+  }, []);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {userSkins ? (userSkins.map((element) => {
-        return (
-          <View style={{ margin: 20 }} key={element.id}>
-            <Text>Your currently condition: {element.skinType} </Text>
-          </View>
-        );
-      })):(
-        <Text>No tracked skin condition</Text>
-      )}
+      {userSkins &&
+        userSkins.map((element) => {
+          return (
+            <View style={{ margin: 20 }} key={element.id}>
+              <Text>{element.skinType}</Text>
+              {skins
+                .filter((skin) => skin.type === element.skinType)
+                .map((skin) => {
+                  return (
+                    <View style={{justifyContent:"center",alignContent:"center"}} key={skin.id}>
+                      <Text>{skin.description}</Text>
+                    </View>
+                  );
+                })}
+            </View>
+          );
+        })}
     </View>
   );
 };
