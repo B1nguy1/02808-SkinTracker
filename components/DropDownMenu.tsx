ae4928cca335fea3866ef74e8937067d19d614bd
Ben <benhv@stud.ntnu.no>
2023-05-08 13:46:06 +0200

    Ready for visualization

--- components/DropDownMenu.tsx
+++ components/DropDownMenu.tsx
@@ -3,14 +3,14 @@ import { SelectList } from "react-native-dropdown-select-list";
 import { IDropDown } from "../utils/interfaces";
 
 
-const DropDownMenu:React.FC<IDropDown> = ({data,setSelected}) => {
+const DropDownMenu:React.FC<IDropDown> = ({data,setSelected,placeholder}) => {
     
     return (
         <SelectList
             setSelected={(val:string) => setSelected(val)}
             data={data}
             search={false}
-            placeholder="Select skin type"
+            placeholder={placeholder}
             dropdownTextStyles={{fontSize:16}}
             boxStyles={{width:"70%",margin:10, marginLeft:50}}
             inputStyles={{width:"90%",borderRadius:0}}
--- screens/DailySkinScreen.tsx
+++ screens/DailySkinScreen.tsx
@@ -1,14 +1,23 @@
 import { getAuth } from "firebase/auth";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import React from "react";
-import { View, Text, Pressable, StyleSheet, Alert } from "react-native";
+import { View, Text, Pressable, StyleSheet, Alert, ScrollView } from "react-native";
 import DropDownMenu from "../components/DropDownMenu";
 import { db } from "../firebase";
-import { skinConditionsData } from "../utils/DropDownMenuData";
+import { conditionsValues, skinConditionsData } from "../utils/DropDownMenuData";
 
 
 const DailySkinScreen = () => {
   const [skinValue, setSkinValue] = React.useState("");
+  const [darkCircleValue,setDarkCircleValue] = React.useState("");
+  const [blackHeadValue, setBlackHeadValue] = React.useState("");
+  const [dehydrationValue, setDehydrationValue] = React.useState("");
+  const [acneValue, setAcneValue] = React.useState("");
+  const [poresValue, setPoresValue] = React.useState("");
+  const [fineLinesValue, setFineLinesValue] = React.useState("");
+  const [spotValue, setSpotValue] = React.useState("");
+  
+  
   const SkinDataRef = collection(db, "skinData");
   const userID = getAuth().currentUser?.uid;
 
@@ -36,11 +45,18 @@ const DailySkinScreen = () => {
       if (userID != null) {
         await addDoc(SkinDataRef, {
           skinType: skinValue,
+          darkCircle: assignScoreToValue(darkCircleValue),
+          blackHead: assignScoreToValue(blackHeadValue),
+          dehydration: assignScoreToValue(dehydrationValue),
+          acne: assignScoreToValue(acneValue),
+          pores: assignScoreToValue(poresValue),
+          fineLines: assignScoreToValue(fineLinesValue),
+          spot: assignScoreToValue(spotValue),
           timeStamp: serverTimestamp(),
           userRef: userID,
         });
         setSkinValue("");
-        Alert.alert("Skin type saved!");
+        Alert.alert("Skin answers saved!");
       }
     } catch (error) {
       console.error(error);
@@ -48,39 +64,77 @@ const DailySkinScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}> How is your skin feeling today? </Text>
+    <ScrollView style={styles.container}>
       <View style={styles.subContainer}>
+      <Text style={styles.textStyle}> How is your skin feeling today? </Text>
         <DropDownMenu
+          placeholder={"Select skin value"}
           data={skinConditionsData}
           setSelected={(value: string) => setSkinValue(value)}
+        />
+        <DropDownMenu
+          placeholder={"Select dark circle"}
+          data={conditionsValues}
+          setSelected={(darkCircle:string) => setDarkCircleValue(darkCircle)}
+        />
+        <DropDownMenu
+          placeholder={"Select black head"}
+          data={conditionsValues}
+          setSelected={(blackHead: string) => setBlackHeadValue(blackHead)}
+        />
+        <DropDownMenu
+          placeholder={"Select dehydration"}
+          data={conditionsValues}
+          setSelected={(dehydration: string) => setDehydrationValue(dehydration)}
+        />
+        <DropDownMenu
+          placeholder={"Select acne"}
+          data={conditionsValues}
+          setSelected={(acne:string) => setAcneValue(acne)}
+        />
+        <DropDownMenu
+          placeholder={"Select enlarged pores"}
+          data={conditionsValues}
+          setSelected={(pores:string) => setPoresValue(pores)}
+        />
+        <DropDownMenu
+          placeholder={"Select fine lines"}
+          data={conditionsValues}
+          setSelected={(fineLines: string) => setFineLinesValue(fineLines)}
+        />
+        <DropDownMenu
+          placeholder={"Select spot"}
+          data={conditionsValues}
+          setSelected={(spot:string) => setSpotValue(spot)}
         />
       </View>
       <Pressable style={styles.button} onPress={addSkinType}>
         <Text style={styles.buttonTextStyle}>Save your choice</Text>
       </Pressable>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flex: 1
   },
   subContainer: {
     margin: 20,
+    justifyContent: "center",
+   
   },
   button: {
     alignItems: "center",
-    borderRadius: 4,
+    borderRadius: 5,
     paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 30,
+    alignSelf:"center",
     backgroundColor: "#f5cac3",
     elevation: 3,
   },
   buttonTextStyle: {
+    fontWeight:"bold",
     lineHeight: 20,
     letterSpacing: 0.2,
     color: "white",
